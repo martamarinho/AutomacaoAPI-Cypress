7918e8270c4120274f1707046da944bd0fbb0241
martamarinho <martamarinhoo@hotmail.com>
2025-03-11 21:42:44 -0300

    adicionando espaco

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -4,6 +4,9 @@ describe('Testando o Status Code da API', () => {
             method: 'GET', 
             url: 'https://petstore.swagger.io/', 
         }).then((response) => {
+           
+
+            
             
             expect(response.status).to.eq(200);
 
