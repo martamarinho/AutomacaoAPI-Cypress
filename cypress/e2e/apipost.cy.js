96ed8914e467291d73f4ccd08418c07ef4f9445d
martamarinho <martamarinhoo@hotmail.com>
2025-03-11 21:53:44 -0300

    .

--- cypress/e2e/apipost.cy.js
+++ cypress/e2e/apipost.cy.js
@@ -14,6 +14,7 @@ describe.only('Testando o envio de um POST', () => {
      
         const requestBody = {
             id: uniqueId, 
+            
             category: {
                 id: 1,
                 name:"",
